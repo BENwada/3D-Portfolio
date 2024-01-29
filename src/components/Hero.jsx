2aad27712f2ba509e3da9c865d857bfbf6bed662
BENwada <domo0103wada@gmail.com>
2024-01-29 18:26:54 +0900

    about section add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,28 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I&lsquo;m <span className="text-[#915eff]">Adrian</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
         </div>
+      </div>
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 3,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
