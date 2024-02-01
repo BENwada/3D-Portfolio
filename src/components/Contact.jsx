0bfc0722f40ce27a548e58397b61d1f0e348da07
BENwada <domo0103wada@gmail.com>
2024-02-01 18:39:36 +0900

    fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,9 +12,47 @@ const Contact = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = () => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // Uuun2YfkXZoXvn0Uk
+    // service_3yjuakh
+    // template_ggznkgq
+
+    emailjs
+      .send(
+        "service_3yjuakh",
+        "template_ggznkgq",
+        {
+          form_name: form.name,
+          to_name: "BEN",
+          from_email: form.email,
+          to_email: "contact@gmail.ccc",
+          message: form.message,
+        },
+        "Uuun2YfkXZoXvn0Uk"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({ name: "", email: "", message: "" });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
